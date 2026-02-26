54785993d67f7d88a10bd109301968c1e044e685
Megan Penn <mepenn23@gmail.com>
2026-02-26 18:00:49 -0500

    outline for cpp

---
+++ cmos.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file cmos.cpp
+ * @author Kennedy Firth and Megan Penn
+ * @brief - cpp file for plagiarism detection program 
+ * @date 2026-02-26
+ */
+
+// include statements
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+
+    // getting txt file
+
+
+    // fingerprinting
+
+
+    // compare fingerprints
+
+
+    // output results to report file
+
+    return 0;
+}
+
+
